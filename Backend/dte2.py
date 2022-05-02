5e3139ee60fbc611fbf1647c60585dbddf566647
André. R <davilacesar125@gmail.com>
2022-05-01 23:12:26 -0600

    Empezando Backend

---
+++ Backend/dte2.py
@@ -0,0 +1,7 @@
+class DTE:
+    def __init__(self,sentimientos,nitE,nitR,valor,codigoA):
+        self.sentimientos = sentimientos
+        self.nitE = nitE
+        self.nitR = nitR
+        self.valor = valor
+        self.codigoA = codigoA
---
+++ diccionario.py
@@ -0,0 +1,12 @@
+class DICCIONARIO:
+    def __init__(self,sentimientos_pos,palabra,sentimientos_neg,empresas_analizar,empresa,nombre,servicio,alias,lista_men,mensajes):
+        self.sentimientos_pos = sentimientos_pos
+        self.palabra = palabra
+        self.sentimientos_neg = sentimientos_neg
+        self.empresas_analizar = empresas_analizar
+        self.empresa = empresa
+        self.nombre = nombre
+        self.servicio = servicio
+        self.alias = alias
+        self.lista_men = lista_men
+        self.mensajes = mensajes
